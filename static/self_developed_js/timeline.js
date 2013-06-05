02343b39f7cf224bf3422c8aa4618cd1fd365add
Casa Taloyum <casatwy@msn.com>
2013-06-05 22:29:00 +0800

    added the function of add an event

--- static/self_developed_js/timeline.js
+++ static/self_developed_js/timeline.js
@@ -5,8 +5,19 @@ $(document).ready(function(){
 
 function TimeLine()
 {
+    function generateTimeLine(){
+        //this is the losted timeline, but it's perfect. i'm gonna retain it for self-appreciate.:D
+//        createStoryJS({
+//            type:"timeline",
+//            width:"1280",
+//            height:"800",
+//            source:"static/example_json.json",
+//            embed_id:"J_timeline",
+//            debug:false
+//        }); 
+    }
+
     this.init = function(){
-        generateTimeLine();
         bindEvent();
     };
 
@@ -14,24 +25,37 @@ function TimeLine()
         $(".J_content").on("click", function(){
             timelineEventClicked($(this));
         });
+
+        $("#J_addEventButton").on("click", function(){
+            addEvent($(this));
+        });
     }
 
-    function generateTimeLine(){
-       //createStoryJS({
-       //    type:"timeline",
-       //    width:"1280",
-       //    height:"800",
-       //    source:"static/example_json.json",
-       //    embed_id:"J_timeline",
-       //    debug:false
-       //}); 
-    }
 
     function timelineEventClicked(actionItem){
         $("#J_detail").html(actionItem.html());
+    }
+
+    function addEvent(actionItem){
+        var Template_add = $("#J_template_add").html();
+
         $.colorbox({
             opacity:0.5,
-            html:actionItem.html()
+            html:Template_add
         });
+
+        $("#cboxLoadedContent .J_addEventConfirmButton").on("click", function(){
+            confirmToAddEvent($(this));
+        });
+    }
+
+    function confirmToAddEvent(actionItem){
+        var inputContent = $("#cboxLoadedContent #J_eventInputContent").val();
+        var Template_event = $("#J_template_event").html();
+
+        Template_event = Template_event.replace("___content___", inputContent);
+        $("#J_timeLine").append(Template_event);
+
+        $.colorbox.close();
     }
 }
