edf365c38199f02db38757d3dc2c7a199e0e3c45
Casa Taloyum <casatwy@msn.com>
2013-07-04 15:34:34 +0800

    added some demo code in package build

--- controllers/PackageBuild.py
+++ controllers/PackageBuild.py
@@ -2,6 +2,7 @@
 
 from config import settings
 import web
+from iostools.command_line import *
 
 data = {'pageIndex':'index'}
 render = settings.render
@@ -18,6 +19,16 @@ class AjaxCheckNewVersion:
 class SelectVersions:
     ''' select the app version and its dependicy's version '''
     def GET(self):
+        print "here i am"
+
+        casa = "casa"
+
+        package(casa)
+        getDependencyArray(casa)
+        getVersionArray(casa, casa)
+        initProject(casa)
+
+        print web.input()
         return render.selectVersions(data=data)
 
     def POST(self):
