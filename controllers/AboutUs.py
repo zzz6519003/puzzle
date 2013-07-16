adb4237271d1319fdcd7989ae01f697905eec224
Casa Taloyum <casatwy@msn.com>
2013-07-16 21:01:31 +0800

    fixed a bug about about

--- controllers/AboutUs.py
+++ controllers/AboutUs.py
@@ -8,7 +8,8 @@ render = settings.render
 class AboutUs:
     ''' 关于我们 '''
     def GET(self):
-        return render.aboutUs("here is about us")
+        return "呵呵"
+        #return render.aboutUs("here is about us")
 
     def POST(self):
         pass
