5353d32bba96507274df7b076207beca1c2b323c
yuetingqian <yuetingqian@anjuke.com>
2013-08-29 17:43:55 +0800

    modify ama db

--- config/settingsReport.py
+++ config/settingsReport.py
@@ -4,14 +4,15 @@ import web
 
 def getConnection():
     '''get connection of database'''
-    db = web.database(dbn='mysql',db='puzzle',user='root',pw='casacasa',host='localhost',port=3306);
+    db = web.database(dbn='mysql',db='puzzle',user='root',pw='anjuke',host='192.168.190.59',port=3306);
     return db
 
 def getConnectionV2(name):
     db = {
-        'puzzle': lambda: web.database(dbn='mysql',db='puzzle',user='root',pw='password',host='192.168.1.194',port=3306),
+        'puzzle': lambda: web.database(dbn='mysql',db='puzzle',user='root',pw='anjuke',host='192.168.190.59',port=3306),
         'pmt':lambda:  web.database(dbn='mysql',db='pmt_new',user='readonly_v2',pw='aNjuKe9dx1Pdw',host='10.10.8.7',port=3309),
         'ibug':lambda:  web.database(dbn='mysql',db='ibug_db',user='readonly_v2',pw='aNjuKe9dx1Pdw',host='10.10.8.7',port=3309),
+        'ama':lambda:  web.database(dbn='mysql',db='Mobile',user='readonly_v2',pw='aNjuKe9dx1Pdw',host='10.10.8.7',port=3309),
         }[name]()
 
     return db
