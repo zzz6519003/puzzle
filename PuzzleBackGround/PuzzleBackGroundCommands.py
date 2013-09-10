fcbc5dec1e735d5821ccdd8d174c3a96ce64b6bb
Casa Taloyum <casatwy@msn.com>
2013-09-10 10:46:22 +0800

    added gearman

--- PuzzleBackGround/PuzzleBackGroundCommands.py
+++ PuzzleBackGround/PuzzleBackGroundCommands.py
@@ -1,8 +1,10 @@
 from gearman import GearmanClient
+import JobList
+import GearmanConfig
 
 def sayHello():
-    newClient = GearmanClient(['127.0.0.1:4730'])
-    currentRequest = newClient.submit_job("echo", "key1", background=True, wait_until_complete=False)
+    newClient = GearmanClient([GearmanConfig.gearmanConnection])
+    currentRequest = newClient.submit_job(JobList.Job_test, "key1", background=True, wait_until_complete=False)
     newResult = currentRequest.result
     print "here is new result in PuzzleBackGroundCommands:"
     print newResult
--- PuzzleBackGround/Workers.py
+++ PuzzleBackGround/Workers.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python
 
-#here is on job execute
-#<GearmanJob connection/handle=(<GearmanConnection 127.0.0.1:4730 connected=True>, 'H:casas-MacBook-Pro.local:1'), task=echo, unique=f688332cb327829633f5107c11250e46, data='this is the first job'>
-#
-#here is call back
-#this is the first job
-
 import gearman
 import time
+import JobList
+import GearmanConfig
 
 class CustomGearmanWorker(gearman.GearmanWorker):
     def on_job_execute(self, currentJob):
         print "here is on job execute"
-
         time.sleep(5.0)
         print "this is the first 5.0"
         time.sleep(5.0)
         print "this is the second 5.0"
-
         return super(CustomGearmanWorker, self).on_job_execute(currentJob)
 
 
@@ -26,6 +20,6 @@ def task_callback(gearmanWorker, job):
     print job.data
     return job.data
 
-newWorker = CustomGearmanWorker(['127.0.0.1:4730'])
-newWorker.register_task("echo", task_callback)
+newWorker = CustomGearmanWorker([GearmanConfig.gearmanConnection])
+newWorker.register_task(JobList.Job_test, task_callback)
 newWorker.work()
