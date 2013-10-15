a5e64ff9365a3e59a449719a480ad10709574291
puzzle <weiyutian@anjuke.com>
2013-10-15 15:18:24 +0800

    finished with the notification

--- controllers/NotificationCenter.py
+++ controllers/NotificationCenter.py
@@ -2,36 +2,13 @@
 
 from config import settings
 from PuzzleBackGround import PuzzleBackGroundCommands
+from iostools.tools.ConfigHelper import ConfigHelper
 render = settings.render
 
 class GitCorpDidMergePullRequest:
-    def translater(paramsForTranslate):
-        """
-            {
-                u'category': u'7',   7 is used for Dailybuild, 8 is used for RC build
-                u'version' : u'5.7',
-                u'appName' : u'haozu',
-                u'projectPath' : u'/var/www/here',
-                u'mailContent' : u'hello world',
-                u'projectId': u'4',
-                u'isDebug': True,
-                u'versionForPackage': u'5.0.1',
-                u'dependencyArray': [
-                    {
-                        u'sha1': u'90123',
-                        u'repoId': u'1',
-                        u'repoName': u'RTNetwork'
-                    },
-                    {
-                        u'sha1': u'90123',
-                        u'repoId': u'2',
-                        u'repoName': u'RTApiProxy'
-                    }
-                ]
-            }
-        """
 
     def GET(self):
-        print "here i am"
+        packageInfo = ConfigHelper().initWithBranchName("develop_p-anjuke_3.1").getConfigData()
+        print packageInfo
         pass
 
