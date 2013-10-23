b81d29fb4b24cab7aa7fbbc68d1c1041cabd8b88
yuetingqian <yuetingqian@gmail.com>
2013-10-23 08:48:50 +0800

    恢复收件人

--- PuzzleBackGround/CalculateCrashCountWorker.py
+++ PuzzleBackGround/CalculateCrashCountWorker.py
@@ -21,7 +21,6 @@ def doWork(gearmanWorker, job):
     params = json.loads(job.data)
     job_start = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     mail_to = ["yuetingqian@anjuke.com","vingowang@anjukeinc.com","clairyin@anjuke.com","angelazhang@anjuke.com"]
-    mail_to = ["yuetingqian@anjuke.com"]
     data['result'] = ''
     error = ''
     start = params['start']
